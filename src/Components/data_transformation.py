e96db57a935bdec70bcd6ab3ee430cc760012d44
Srinivas Balivada <balivada987@gmail.com>
2024-12-15 23:59:15 +0530

    Model Implemeted and hyperparameter tuned

--- src/Components/data_transformation.py
+++ src/Components/data_transformation.py
@@ -1,47 +1,87 @@
-
 import sys
 from dataclasses import dataclass
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import SimpleImputer,StandardScaler,OneHotEncoder
-from sklearn.preprocessing import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
 from src.exceptions import CustomException
 from src.logger import logging
+from sklearn.compose import ColumnTransformer
 import os
+from src.utils import save_object
+from sklearn.impute import SimpleImputer
 
 @dataclass
 class DataTransformationConfig:
-    preprocess_obj_file_path=os.path.join('artifacts', 'preprocess.pkl')
-
+    preprocess_obj_file_path = os.path.join('artifacts', 'preprocess.pkl')
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
-    
+        self.data_transformation_config = DataTransformationConfig()
+
     def get_data_transformation_object(self):
         try:
-            numeric_columns=[]
-            cat_columns=[]
+            numeric_columns = ["reading_score", "writing_score"]
+            cat_columns = ['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch','test_preparation_course']
+
             # Numerical pipeline
             num_pipeline = Pipeline(steps=[
-                 ("imputer", SimpleImputer(strategy="median")),
-                 ("scaler", StandardScaler())])
+                ("imputer", SimpleImputer(strategy="median")),
+                ("scaler", StandardScaler())
+            ])
 
             # Categorical pipeline
-            cat_pipeline = Pipeline(
-                steps=[
-                    ("imputer", SimpleImputer(strategy="most_frequent")),  # "mode" is not valid, use "most_frequent"
-                    ("onehot_encode", OneHotEncoder()),
-                    ("scaler", StandardScaler())
+            cat_pipeline = Pipeline(steps=[
+                ("imputer", SimpleImputer(strategy="most_frequent")),
+                ("onehot_encode", OneHotEncoder())
+            ])
+
+            logging.info("Reading of numeric and categorical pipelines done")
+
+            # Correct ColumnTransformer initialization
+            preprocessing = ColumnTransformer(
+                transformers=[
+                    ("num_pipeline", num_pipeline, numeric_columns),
+                    ("cat_pipeline", cat_pipeline, cat_columns)
                 ]
-            ) 
+            )
 
-        
-                       
-        except CustomException as e:
-            pass
-
-
-        
-
+            logging.info("Transformation on two columns is done")
             
+            return preprocessing
+
+        except CustomException as e:
+            raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("train test file reading done in initiate_data_transformation")
+            preprocessing_obj = self.get_data_transformation_object()
+
+            target_column_name = "math_score"
+            numerical_column_name = ["writing_score", "reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            print(input_feature_train_df)
+            logging.info("target Column Dropped in train and test")
+            
+            scaled_train = preprocessing_obj.fit_transform(input_feature_train_df)
+            scaled_test = preprocessing_obj.transform(input_feature_test_df)  # Use transform instead of fit_transform for test data
+            print(scaled_train)
+            print(scaled_test)
+            logging.info("train and test executed")
+
+            train_df = np.c_[scaled_train, np.array(train_df[target_column_name])]
+            test_df = np.c_[scaled_test, np.array(test_df[target_column_name])]
+            logging.info("merged train, test with target")
+
+            save_object(file_path=self.data_transformation_config.preprocess_obj_file_path, obj=preprocessing_obj)
+            logging.info("saved pipeline dill file")
+
+            return train_df, test_df, self.data_transformation_config.preprocess_obj_file_path
+
+        except Exception as e:
+            raise CustomException(e, sys)
---
+++ src/utils.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import dill
+from src.logger import logging
+
+from src.exceptions import CustomException
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+import warnings
+
+def save_object(file_path, obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+            logging.info(" pickle file dumped")
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train,X_test,y_test, models, params:dict):
+    try:
+        report= {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param=list(params.values())[i]
+            X_train=X_train
+            y_train=y_train
+            #rs=RandomizedSearchCV(model,param)
+            model.fit(X_train,y_train) 
+            #model.set_params(**rs.best_params_) # Train model
+
+            # Make predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            # Evaluate Train and Test dataset
+            model_train_r2 = r2_score(y_test[:199],y_test_pred[:199])
+            report[list(models.keys())[i]]= model_train_r2
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
